890f1f7b95e74d7ca488d93746c7cec21bfc0fba
Agney saidas <52481643+agneysaidas@users.noreply.github.com>
2025-04-17 17:07:04 +0530

    Update caesar _cipher2.py

--- Caesar Cipher/caesar _cipher2.py
+++ Caesar Cipher/caesar _cipher2.py
@@ -7,9 +7,11 @@ shift = int(input("Type the shift number:\n"))
 def caesar(original_text , shift_amount , encode_or_decode):
   cipher_text = ""
   if encode_or_decode == "decode":
-    shift amount *= -1
+    shift_amount *= -1
   for letter in original_text:
     shifted_position = alphabet.index(letter) + shift_amount
     shifted_position %= len(alphabet)
     cipher_text += alphabet[shifted_position]
   print(f"Here is the encoded result: {cipher_text}")
+
+caesar(original_text=text, shift_amount=shift, encode_or_decode = direction)
